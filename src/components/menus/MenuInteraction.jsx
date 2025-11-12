e177b21e08b78f96434c64a6fc37626c10f549f6
areeshalam <areeshpersonal5@gmail.com>
2025-11-12 23:53:16 +1100

    Updated Menu Interaction —MENUS— component JSX and Styles

--- src/components/menus/MenuInteraction.jsx
+++ src/components/menus/MenuInteraction.jsx
@@ -1,49 +1,47 @@
 import { TouchableOpacity, View } from "react-native";
 import { MenuInteractionStyles } from "../../styles/MenuInteractionStyles";
+import { Ionicons } from "@expo/vector-icons";
 
-export const MenuInteraction = ({
-  likeIcon,
-  dislikeIcon,
-  shareIcon,
-  commentIcon,
-  repostIcon,
-  moreOptionsIcon,
-  containerStyles,
-  columnMainIconStyles,
-}) => {
+export const MenuInteraction = ({ containerStyles, columnMainIconStyles }) => {
   return (
     <View style={[MenuInteractionStyles.container, containerStyles]}>
       <View
         style={[MenuInteractionStyles.column_mainIcons, columnMainIconStyles]}
       >
-        {likeIcon && (
-          <TouchableOpacity>
-            <View style={MenuInteractionStyles.icon}>{likeIcon}</View>
-          </TouchableOpacity>
-        )}
-        {dislikeIcon && (
-          <TouchableOpacity>
-            <View style={MenuInteractionStyles.icon}>{dislikeIcon}</View>
-          </TouchableOpacity>
-        )}
-        {shareIcon && (
-          <TouchableOpacity>
-            <View style={MenuInteractionStyles.icon}>{shareIcon}</View>
-          </TouchableOpacity>
-        )}
-        {commentIcon && (
-          <TouchableOpacity>
-            <View style={MenuInteractionStyles.icon}>{commentIcon}</View>
-          </TouchableOpacity>
-        )}
-        {repostIcon && (
-          <TouchableOpacity>
-            <View style={MenuInteractionStyles.icon}>{repostIcon}</View>
-          </TouchableOpacity>
-        )}
+        <TouchableOpacity>
+          <View style={MenuInteractionStyles.icon}>
+            <Ionicons name="arrow-redo" size={24} color="white" />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <View style={MenuInteractionStyles.icon}>
+            <Ionicons name="chatbubble-ellipses" size={24} color="white" />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <View style={MenuInteractionStyles.icon}>
+            <Ionicons name="repeat-outline" size={24} color="white" />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <View style={MenuInteractionStyles.icon}>
+            <Ionicons name="thumbs-up-outline" size={24} color="white" />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <View style={MenuInteractionStyles.icon}>
+            <Ionicons name="thumbs-down-outline" size={24} color="white" />
+          </View>
+        </TouchableOpacity>
       </View>
 
-      {moreOptionsIcon && <View>{moreOptionsIcon}</View>}
+      <TouchableOpacity>
+        <Ionicons name="ellipsis-vertical-outline" size={24} color="white" />
+      </TouchableOpacity>
     </View>
   );
 };
